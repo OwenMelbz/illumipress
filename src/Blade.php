587fcaba6fa90873225e850463935178613285c4
Owen <owenmelbz@gmail.com>
2018-04-02 13:21:54 +0100

    started adding blade loader

--- src/Blade.php
+++ src/Blade.php
@@ -1,5 +1,44 @@
 <?php
-array_map(function ($file) {
-    $file = "globals/{$file}.php";
-    // require_once($file);
-}, ['helpers', 'setup', 'filters']);
+
+namespace OwenMelbz\IllumiPress;
+
+class BladeLoader
+{
+    public function __construct()
+    {
+        $this->loadVendorFiles();
+
+        return $this;
+    }
+
+    public function loadVendorFiles()
+    {
+        $vendorDir = realpath(__DIR__ . '/../../../');
+        $bladeRunnerDir = $vendorDir . '/ekandreas/bladerunner';
+
+        foreach ($this->getFileList() as $fileName) {
+            $filePath = "{$bladeRunnerDir}/{$fileName}.php";
+            require_once($filePath);
+        }
+
+        return $this;
+    }
+
+    public function getFileList()
+    {
+        return [
+            'src/BladeProvider',
+            'src/Blade',
+            'src/Config',
+            'src/Container',
+            'src/Controller',
+            'src/ControllerDebug',
+            'src/FileViewFinder',
+            'src/Repository',
+            'globals/helpers',
+            'globals/setup',
+            'globals/filters',
+        ];
+    }
+
+}
